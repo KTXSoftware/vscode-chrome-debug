1d0305b82d5a622df53e40485cc9278cd11606fd
Robert Konrad <git2016@robdangero.us>
2016-08-23 17:02:56 +0200

    Adjust for vscode's build structure

--- src/chromeDebug.ts
+++ src/chromeDebug.ts
@@ -2,15 +2,15 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-const core = require('../../core');
+const core = require('../core');
 import * as path from 'path';
 
 // Start a ChromeDebugSession configured to only match 'page' targets, which are Chrome tabs
 core.ChromeDebugSession.run(core.ChromeDebugSession.getSession(
     {
         targetFilter: target => target && (!target.type || target.type === 'page'),
-        logFileDirectory: path.resolve(__dirname, '../../')
+        logFileDirectory: path.resolve(__dirname, '../')
     }));
 
 /* tslint:disable:no-var-requires */
-core.logger.log('debugger-for-chrome: ' + require('../../package.json').version);
+core.logger.log('debugger-for-chrome: ' + require('../package.json').version);
